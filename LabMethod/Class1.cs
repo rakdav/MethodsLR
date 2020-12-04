2ecc04c3f64db2ab9a7a83d0fb43ae8a44c0957f
Andrey Dubinin <rakdav@inbox.ru>
2020-12-04 18:15:59 +0200

    Добавьте файлы проекта.

---
+++ LabMethod/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LabMethod
+{
+    public class Class1
+    {
+        public static double[] SummaNeg(double[,] mas)
+        {
+            int rows = mas.GetUpperBound(0) + 1;
+            int columns = mas.Length / rows;
+            double[] m = new double[rows];
+            for(int i=0;i<rows;i++)
+            {
+                double s = 0;
+                for(int j=0;j<columns;j++)
+                {
+                    if (mas[i, j] < 0) s += mas[i, j];
+                }
+                m[i] = s;
+            }
+            return m;
+        }
+    }
+}
---
+++ MethodsLR/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LabMethod;
+
+namespace MethodsLR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //средний уровень
+                int n8;
+                
+                if(int.TryParse(Console.ReadLine(),out n8))
+                {
+                    Console.WriteLine(ToDecimal(n8));
+                }
+                //начальный уровень библиотеки
+                int n = int.Parse(Console.ReadLine());
+                int m = int.Parse(Console.ReadLine());
+                Random rnd = new Random();
+                double[,] mas = new double[n,m];
+                for(int i=0;i<n;i++)
+                {
+                    for(int j=0;j<m;j++)
+                    {
+                        mas[i, j] = rnd.Next(50) - 25;
+                        Console.Write(mas[i,j]+" ");
+                    }
+                    Console.WriteLine();
+                }
+                double[] onem = Class1.SummaNeg(mas);
+                for (int i = 0; i < onem.Length; i++) Console.Write(onem[i]+" ");
+                Console.WriteLine();
+                Console.ReadKey();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        //Средний уровень
+        static int ToDecimal(int n)
+        {
+            int i = 0;
+            int s = 0;
+            while(n!=0)
+            {
+                int m = n % 10;
+                s += m * Step8(i);
+                i++;
+                n /= 10;
+            }
+            return s;
+        }
+        static int Step8(int n)
+        {
+            int s = 1;
+            for(int i=1;i<=n;i++)
+            {
+                s *= 8;
+            }
+            return s;
+        }
+    }
+}
